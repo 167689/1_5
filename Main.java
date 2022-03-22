03aa5c6241a0eaae167399b82e3de61a6d5413ee
167689 <101180601+167689@users.noreply.github.com>
2022-03-22 10:52:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    for (byte i=50;i<=100;i++){
+      if (i%2==0){
+        
+      }
+      else {
+        System.out.println(i);
+      }
+    }
+    
+    
+  }
+}
